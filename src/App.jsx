fc3f39e4a2010058cdb50407fcd5c05c71eaa1cc
Steven Jimenez <stevenjimenez00@hotmail.com>
2023-09-01 11:59:10 -0400

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ClientesRegistro from "./components/ClientesRegistro";
-import RegistroVehiculo from "./components/RegistroVehiculo/registroVehiculo";
-import Lista from "./components/lista/lista";
 
 function App() {
   return (
     <>
-      <Lista />
+      <ClientesRegistro />
     </>
   );
 }
