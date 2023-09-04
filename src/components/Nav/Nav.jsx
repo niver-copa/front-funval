efa9090ca8ea6507816fd11fb21411f1545844fc
SirBeho <benjamin.tavarez.98@gmail.com>
2023-09-04 15:42:53 -0400

    nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,13 @@
-const Nav = () => {
-  return (
-    <>
- 
-  
-    <nav className="z-10 top-0 fixed w-screen bg-blue-950 max-w-screen flex flex-wrap items-center justify-between text-white p-2 px-6">
-      <div className="flex gap-4 items-center">
+'use client';
 
-      
-      <a href="/" className="flex items-center ">
-        <img
+import { Dropdown, Navbar } from 'flowbite-react';
+
+export default function Nav() {
+  return (
+    <nav className="z-10 top-0 fixed w-screen bg-blue-950 max-w-screen flex flex-wrap items-center justify-between text-white h-20 py-0 px-6" >
+      <div className="flex gap-2 items-center h-full font-medium ">
+      <Navbar.Brand href="/" className="flex items-center ">
+      <img
           src="https://flowbite.com/docs/images/logo.svg"
           className="h-8 mr-3"
           alt="Flowbite Logo"
@@ -16,112 +15,30 @@ const Nav = () => {
         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
           Flowbite
         </span>
-      </a>
-     
-      <div className=" w-auto" id="navbar-dropdown">
-        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-         
-          <li>
-            <button
-              id="dropdownNavbarLink"
-              data-dropdown-toggle="dropdownNavbar"
-              className="flex items-center justify-between w-full "
-            >
-              Dropdown{" "}
-              <svg
-                className="w-2.5 h-2.5 ml-2.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
-            </button>
-            {/* Dropdown menu */}
-            <div
-              id="dropdownNavbar"
-              className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
-            >
-              <ul
-                className="py-2 text-sm text-gray-700 dark:text-gray-400"
-                aria-labelledby="dropdownLargeButton"
-              >
-                
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Dashboard
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Earnings
-                  </a>
-                </li>
-              </ul>
-              <div className="py-1">
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
-                >
-                  Sign out
-                </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <a
-              href="/cardealer"
-              className="block  text-white  bg-transparent "
-              aria-current="page"
-            >
-              Listado
-            </a>
-          </li>
+      </Navbar.Brand>
 
-          <li>
-            <a
-              href="/clientes"
-              className="block  text-white  bg-transparent "
-              aria-current="page"
-            >
-              Clientes
-            </a>
-          </li>
-          <li>
-            <a
-              href="/registro"
-              className="block  text-white  bg-transparent "
-              aria-current="page"
-            >
-              Vehiculos
-            </a>
-          </li>
-         
-        </ul>
-      </div>
-      </div>
-      <div className="me-4">
+      <div className="flex h-20 p-4 items-center text-white   hover:bg-blue-900">
+        <Dropdown inline  label={"Mantenimientos "} className="flex h-20 p-4 items-center">
+            Dashboard
+            Settings
+            Earnings
+          <Dropdown.Divider />
+            Sign out
+        </Dropdown>
+        </div>
+       <ul className='flex h-full items-center'>
+        <Navbar.Link  href="/cardealer" className="flex h-20 p-4 items-center text-white   hover:bg-blue-900">
+          Listado
+        </Navbar.Link>
+        <Navbar.Link  href="/clientes" className="flex h-20 p-4 items-center  text-white   hover:bg-blue-900">
+          Clientes
+        </Navbar.Link>
+        <Navbar.Link  href="/registro" className="flex h-20 p-4 items-center text-white  hover:bg-blue-900">
+          Vehiculos
+        </Navbar.Link>
+    </ul>
+</div>
+ <div className="me-4">
 
      
         <img
@@ -130,11 +47,12 @@ const Nav = () => {
           alt="user"
         />
          </div>
+     
+   
+     
+ 
     </nav>
+  )
+}
 
-</>
 
-  );
-};
-
-export default Nav;
