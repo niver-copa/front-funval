b1bcc4bfcd7ee53d8dcf4f265d10674d04e12b8e
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2023-09-04 18:46:28 -0300

    api car details

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,20 @@ import Nav from "./components/Nav/Nav";
 import Lista from "./components/lista/lista";
 import Marcas from "./components/Marcas/page";
 
-
-
-
 function App() {
   return (
     <BrowserRouter>
-      <Nav/>
-      <Routes >
+      <Nav />
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/clientes" element={<ClientesRegistro />} />
         <Route path="/registro" element={<RegistroVehiculo />} />
         <Route path="/marcas" element={<Lista />} />
         <Route path="/cardealer" element={<CarHome />} />
-        <Route path="/cardealer/:name" element={<CardDetails />} />
+        <Route path="/cardealer/:id" element={<CardDetails />} />
         <Route path="/marcas" element={<Marcas />} />
       </Routes>
-     
     </BrowserRouter>
-   
   );
 }
 
