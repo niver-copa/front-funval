60c258cbfa95dbe6a774f461b9c0e94c28fc57f6
Carlos Alvarado <carlosjam10@gmail.com>
2023-09-04 17:33:38 -0500

    update

--- src/components/formClientes/FormClientes.jsx
+++ src/components/formClientes/FormClientes.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 function FormClientes() {
   const [formData, setFormData] = useState({
     referencias: "",
-    histdecompras: "",
-    niveldesatisfacion: "",
+    historial_de_compras: "",
+    nivel_de_satisfaccion: "",
     observaciones: "",
   });
   const handleInputChange = (event) => {
@@ -20,9 +20,7 @@ function FormClientes() {
 
   return (
     <div className="max-w-md mx-auto mt-10 p-4 bg-white shadow-md rounded-md ">
-
       <div onSubmit={handleSubmit} className="grid grid-cols-3 gap-3 w-160">
-
         <div className="mb-4">
           <label htmlFor="referencias" className="block font-medium mb-1">
             Referencias
@@ -36,11 +34,14 @@ function FormClientes() {
           ></textarea>
         </div>
         <div className="mb-4">
-          <label htmlFor="historialCompras" className="block font-medium mb-1">
+          <label
+            htmlFor="historial_de_compras"
+            className="block font-medium mb-1"
+          >
             Hist. de Compras
           </label>
           <textarea
-            id="historialCompras"
+            id="historial_de_compras"
             value={formData.histdecompras}
             onChange={handleInputChange}
             className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300 resize-none"
@@ -48,32 +49,36 @@ function FormClientes() {
           ></textarea>
         </div>
         <div className="mb-4">
-          <label htmlFor="nivelSatisfaccion" className="block font-medium mb-1">
+          <label
+            htmlFor="nivel_de_satisfaccion"
+            className="block font-medium mb-1"
+          >
             Nivel Satisfacción
           </label>
           <input
             type="number"
-            id="nivelSatisfaccion"
+            id="nivel_de_satisfaccion"
             value={formData.niveldesatisfacion}
             onChange={handleInputChange}
             className="w-full px-3 py-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="observaciones" className="block font-medium mb-1">
+          <label
+            htmlFor="comentarios_observaciones"
+            className="block font-medium mb-1"
+          >
             Observaciones
           </label>
           <textarea
-            id="observaciones"
+            id="comentarios_observaciones"
             value={formData.observaciones}
             onChange={handleInputChange}
             className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300 resize-none"
             rows="3"
           ></textarea>
         </div>
-
       </div>
-
     </div>
   );
 }
