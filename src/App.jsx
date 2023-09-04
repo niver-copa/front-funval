d174f48d5ea6a1aacb86caa5695a0252a0f84c09
Carlos Alvarado <carlosjam10@gmail.com>
2023-09-04 14:14:58 -0500

    corregido formulario

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { CarHome } from "./components/CarHome/CarHome";
 import CardDetails from "./components/CarHome/CarDetails";
-import ClientesRegistro from "./components/ClientesRegistro";
+import FormClientes from "./components/formClientes/FormClientes";
 import RegistroVehiculo from "./components/RegistroVehiculo/registroVehiculo";
 import CarNav from "./components/Nav/Nav";
 import Lista from "./components/lista/lista";
 import Marcas from "./components/Marcas/page";
+import FormRegistros from "./components/formRegistros/FormRegistros";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       <CarNav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/clientes" element={<ClientesRegistro />} />
+        <Route path="/clientes" element={<FormRegistros />} />
         <Route path="/registro" element={<RegistroVehiculo />} />
         <Route path="/marcas" element={<Lista />} />
         <Route path="/cardealer" element={<CarHome />} />
