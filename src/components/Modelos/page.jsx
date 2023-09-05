4c745c9c9da3fedd7669984e9c144c505d19e325
Roman Franco <namor18@gmail.com>
2023-09-05 09:44:39 -0400

    agregado componente modelos, corregido errores en codigo

--- src/components/Modelos/page.jsx
+++ src/components/Modelos/page.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Modelos() {
@@ -7,10 +7,11 @@ export default function Modelos() {
   const [modelos, setModelos] = useState([]);
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/modelos')
+    axios
+      .get("http://127.0.0.1:8000/api/modelos")
       .then((response) => {
         setModelos(response.data);
-        console.log(response.data)
+        console.log(response.data);
       })
       .catch((error) => {
         console.error(error);
@@ -22,8 +23,17 @@ export default function Modelos() {
         <main className="container mx-auto py-8">
           <div className="flex justify-center">
             <div className="w-full md:w-1/2 p-4">
-              <label htmlFor="search" className="block text-lg  mb-2 font-medium">Search:</label>
-              <input id="search" type="text" className="w-full px-4 py-2 border border-blue-900 rounded-md" />
+              <label
+                htmlFor="search"
+                className="block text-lg  mb-2 font-medium"
+              >
+                Search:
+              </label>
+              <input
+                id="search"
+                type="text"
+                className="w-full px-4 py-2 border border-blue-900 rounded-md"
+              />
             </div>
             <button
               onClick={() => setIsModalOpen(!isModalOpen)}
@@ -43,10 +53,10 @@ export default function Modelos() {
                 </tr>
               </thead>
               <tbody>
-                {marcas.map((modelos) => (
+                {modelos.map((modelos) => (
                   <tr key={modelos.id} className="border-b border-gray-300">
-                      <td className="py-4 px-6">{modelos.id}</td>
-                      <td className="py-4 px-6">{modelos.nombre}</td>
+                    <td className="py-4 px-6">{modelos.id}</td>
+                    <td className="py-4 px-6">{modelos.nombre}</td>
                   </tr>
                 ))}
               </tbody>
@@ -64,32 +74,62 @@ export default function Modelos() {
         >
           <div className="bg-white rounded-lg shadow dark:bg-gray-700 w-full md:max-w-md">
             <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button 
-                type="button" 
+              <button
+                type="button"
                 onClick={() => setIsModalOpen(false)}
                 className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
                 </svg>
                 <span className="sr-only">Close modal</span>
-            </button>
-                <div className="px-6 py-6 lg:px-8">
-                    <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Agregar Marca</h3>
-                    <form className="space-y-6" action="#">
-                        <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
-                            <input type="text" name="nueva-marca" id="nueva-marca" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nissan" required />
-                        </div>
-                       
-                        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Guardar Marca</button>
-                    </form>
-                </div>
+              </button>
+              <div className="px-6 py-6 lg:px-8">
+                <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+                  Agregar Modelo
+                </h3>
+                <form className="space-y-6" action="#">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Nombre
+                    </label>
+                    <input
+                      type="text"
+                      name="nuevo-modelo"
+                      id="nuevo-modelo"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      placeholder="Nissan"
+                      required
+                    />
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Guardar Modelo
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       )}
-
     </>
-  )
+  );
 }
